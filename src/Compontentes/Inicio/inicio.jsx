dd9669ee742f70a6140477676e536eb7ab90dc9a
Nick_STM <mancillanixon7@gmail.com>
2024-05-28 09:51:55 -0500

    Sincronizando

--- src/Compontentes/Inicio/inicio.jsx
+++ src/Compontentes/Inicio/inicio.jsx
@@ -21,7 +21,8 @@ const Inicio = () => {
 
     return (
         <div className='inicio'>
-            <h3 className='Inst'>NIXON INSTITUTO DE EDUCACION SUPERIOR TECNOLOGICO PUBLICO SUIZA</h3>
+            <h1>ROMER ALEJANDRO</h1>
+            <h3 className='Inst'>INSTITUTO DE EDUCACION SUPERIOR TECNOLOGICO PUBLICO SUIZA</h3>
             <div className='subInicio'>
                 <h1>DESARROLLO DE SISTEMAS DE INFORMACION - SUIZA</h1>
                 <h3>Cada línea de código que escribes es un paso hacia la transformación digital. Abraza el poder de la innovación y deja que tu pasión por el desarrollo de sistemas te guíe hacia el éxito.</h3>
