5724d1558766954b5c50a0a2456a563f2d2900be
Nicko-rgb <mancillanixon7@gmail.com>
2024-07-06 22:20:23 -0500

    subiendo 6/7/24

--- src/Compontentes/Inicio/Coding.jsx
+++ src/Compontentes/Inicio/Coding.jsx
@@ -5,18 +5,17 @@ const Coding = () => {
     const [code, setCode] = useState('');
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    const codeExample = `
-    1   import Data from './data.js'
-    2   const Coding = (data) => {
-    3       const User = {
-    4           name: 'John',
-    5           email: 'jhon54@gmail.com',
-    6           age: 30,
-    7       }
-    8       Data.push(User)
-    9       console.log(Data)
-    10  }
-    `;
+    const codeExample = 
+`  1   import Data from './data.js'
+  2   const Coding = (data) => {
+  3       const User = {
+  4           name: 'Juan',
+  5           email: 'juan54@gmail.com',
+  6           age: 30,
+  7       }
+  8       Data.push(User)
+  9       console.log(Data)
+  10  }`;
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -36,7 +35,7 @@ const Coding = () => {
     return (
         <div className="code-example">
             <pre>
-                <code>{code}</code>
+                <code>{code}<span>|</span></code>
             </pre>
         </div>
     );
--- src/Compontentes/Navegador/Navegador.jsx
+++ src/Compontentes/Navegador/Navegador.jsx
@@ -2,6 +2,11 @@
 import './navegador.css'
 import { Link } from "react-router-dom"
 import logo from '../../IMG/logo.png'
+import { IoHome } from "react-icons/io5";
+import { MdOutlinePublic } from "react-icons/md";
+import { TbInfoCircleFilled } from "react-icons/tb";
+import { FaNewspaper } from "react-icons/fa6";
+
 const Navegador = () => {
     return (
         <div className='navegador'>
@@ -10,10 +15,10 @@ const Navegador = () => {
                     <img src={logo} alt="logo" />
                 </Link>
                 <nav>
-                    <Link className="link link1" to='/'>Inicio</Link>
-                    <Link className="link link2" to='/publicacion'>Publicaciones</Link>
-                    <Link className="link link3" to='/dsi'>DSI</Link>
-                    <Link className="link link4" to='/periodico_mural'>P. Mural</Link>
+                    <Link className="link link1" to='/'><IoHome className='navIco'/> Inicio</Link>
+                    <Link className="link link2" to='/publicacion'><MdOutlinePublic className='navIco'/> Publicaciones</Link>
+                    <Link className="link link3" to='/dsi'><TbInfoCircleFilled className='navIco'/> DSI</Link>
+                    <Link className="link link4" to='/periodico_mural'><FaNewspaper className='navIco'/> P. Mural</Link>
                 </nav>
                 <div>
                     <Link to='/login'>
